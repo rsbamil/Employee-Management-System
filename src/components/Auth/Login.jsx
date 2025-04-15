191a40ce29741ac141c3c3c685852024e600e08b
Rohit Kumar <rsbamil66@gamil.com>
2025-04-15 18:52:16 +0530

    upto login page

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("email", email);
+    console.log("password", password);
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+        <form
+          onSubmit={(e) => submitHandler(e)}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            className=" text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl outline-none bg-transparent placeholder:text-zinc-500 "
+            type="email"
+            placeholder="email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className=" text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl outline-none bg-transparent placeholder:text-zinc-500 mt-4"
+            type="password"
+            placeholder="enter password"
+          />
+          <button className=" text-white  bg-emerald-600 rounded-full py-3 px-5 text-xl outline-none placeholder:text-zinc-500 mt-4">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
